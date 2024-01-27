eabbff2321845d0a5227cae8f10acebe6b5468c0
Lorenzo Fontoura <lorenzo@nioel.com>
2024-01-27 16:21:20 +1000

    feat: calculate max component width in root fn

--- src/render.rs
+++ src/render.rs
@@ -1,4 +1,5 @@
 use crate::{Component, ComponentKind, Diagram};
+use std::io::empty;
 use std::sync::Barrier;
 
 const COMPONENT_HEIGHT: f64 = 50.0;
@@ -51,7 +52,8 @@ where
         .iter()
         .filter(|c| c.kind == ComponentKind::Consequence)
         .collect::<Vec<&Component>>();
-    let (r, canvas) = setup_canvas(r, &causes, &consequences);
+    let max_component_box_width = calculate_max_components_box_width(&causes, &consequences);
+    let (r, canvas) = setup_canvas(r, &causes, &consequences, max_component_box_width);
     // Draw a border around the canvas, mostly for debugging purposes.
     let r = r.draw_rectangle(&Rectangle {
         centre: Vector2 {
@@ -61,8 +63,20 @@ where
         width: canvas.width,
         height: canvas.height,
     });
-    let r = render_components(r, &causes, ComponentKind::Cause, &canvas);
-    let r = render_components(r, &consequences, ComponentKind::Consequence, &canvas);
+    let r = render_components(
+        r,
+        &causes,
+        ComponentKind::Cause,
+        &canvas,
+        max_component_box_width,
+    );
+    let r = render_components(
+        r,
+        &consequences,
+        ComponentKind::Consequence,
+        &canvas,
+        max_component_box_width,
+    );
     let r = render_event_circle(r, &diagram, &canvas);
     r.into_bytes()
 }
@@ -71,8 +85,7 @@ fn render_event_circle<R>(r: R, diagram: &Diagram, canvas: &Canvas) -> R
 where
     R: Renderer,
 {
-    let width = text_width(&diagram.event);
-    let radius = width / 2.0;
+    let radius = calculate_event_circle_radius(&diagram.event);
     let r = r.draw_circle(
         radius,
         &Vector2 {
@@ -94,7 +107,17 @@ where
     r
 }
 
-fn setup_canvas<'a, R>(r: R, causes: &[&Component], consequences: &[&Component]) -> (R, Canvas)
+fn calculate_event_circle_radius(event: &str) -> f64 {
+    let width = text_width(&event);
+    width / 2.0
+}
+
+fn setup_canvas<'a, R>(
+    r: R,
+    causes: &[&Component],
+    consequences: &[&Component],
+    max_component_box_width: f64,
+) -> (R, Canvas)
 where
     R: Renderer,
 {
@@ -123,8 +146,35 @@ fn calculate_barriers_container_width(barriers: &[&Barrier]) -> f64 {
     barriers_count * BARRIER_WIDTH * ((barriers_count - 1.0) * BARRIER_MARGIN_RIGHT) + padding
 }
 
-fn calculate_canvas_width(components: &[&Component], barriers_container_width: f64) -> f64 {
-    0.0
+fn calculate_canvas_width(
+    diagram: &Diagram,
+    max_component_box_width: f64,
+    max_barriers_container_width: f64,
+) -> f64 {
+    calculate_event_circle_radius(&diagram.event)
+        + (max_component_box_width * 2.0)
+        + (max_barriers_container_width * 2.0)
+}
+
+fn calculate_max_barriers_container_width(a: &[&Barrier], b: &[&Barrier]) -> f64 {
+    let aw = calculate_barriers_container_width(a);
+    let bw = calculate_barriers_container_width(b);
+    aw.max(bw)
+}
+
+fn calculate_max_components_box_width(a: &[&Component], b: &[&Component]) -> f64 {
+    let aw = calculate_max_component_box_width(a);
+    let bw = calculate_max_component_box_width(b);
+    aw.max(bw)
+}
+
+fn calculate_max_component_box_width(components: &[&Component]) -> f64 {
+    components
+        .iter()
+        .map(|c| text_width(&c.name) as u32)
+        .max()
+        .map(|v| v as f64)
+        .unwrap_or(0.0)
 }
 
 fn render_components<'a, R>(
@@ -132,6 +182,7 @@ fn render_components<'a, R>(
     components: &[&Component],
     kind: ComponentKind,
     canvas: &Canvas,
+    max_component_box_width: f64,
 ) -> R
 where
     R: Renderer,
@@ -143,25 +194,18 @@ where
         canvas.consequences_container_height
     };
     let components_container_top = (canvas.height / 2.0) - (container_height / 2.0);
-    let longest_box_width = components
-        .iter()
-        .map(|c| text_width(&c.name) as u32)
-        .max()
-        .map(|v| v as f64)
-        .unwrap_or(0.0);
     for (i, component) in components.iter().enumerate().map(|(i, c)| (i as f64, c)) {
         let height = 50.0;
         let y_relative = i * COMPONENT_HEIGHT + (i * COMPONENT_MARGIN_BOTTOM);
         let y = components_container_top + y_relative + (height / 2.0);
-        let box_width = longest_box_width;
         let x = if is_cause {
-            (box_width / 2.0) + 10.0
+            (max_component_box_width / 2.0) + 10.0
         } else {
-            canvas.width - (box_width / 2.0) - 10.0
+            canvas.width - (max_component_box_width / 2.0) - 10.0
         };
         let rectangle = Rectangle {
             centre: Vector2 { x, y },
-            width: box_width,
+            width: max_component_box_width,
             height,
         };
         r = r.draw_text_with_rectangle(&component.name, &rectangle);
